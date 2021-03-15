06591a8b938a938537e7cadf328696cd99396159
Bouncyyahomie <thornny.s@gmail.com>
2021-03-15 21:36:26 +0700

    deleted un use file

--- s.py
+++
@@ -1,7 +0,0 @@
-from tkinter import * 
-root = Tk()  
-canv = Canvas(root, width=80, height=80, bg='white') 
-canv.grid(row=2, column=3)  
-img = PhotoImage(file=r"images/dot.gif") 
-canv.create_image(20, 20, anchor=NW, image=img)  
-mainloop()
